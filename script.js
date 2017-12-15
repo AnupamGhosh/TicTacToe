c98ac308c3cb17533b45aa1d9c9db30de7db342f
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2017-12-15 17:17:09 +0530

    removed event delegation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 (function() {
+	var ROWS = 3;
+	var COLS = 3;
 	var CROSS = 1;
 	var CIRCL = 0;
 	var XELEM = '<div class="cross"><div class="lt"></div><div class="rt"></div></div>';
@@ -8,36 +10,36 @@
 	var move = CIRCL;
 	var nineSq = document.getElementById("nine-box");
 	var sInput = document.getElementsByClassName("switch")[0];
+	var boxes = document.querySelectorAll('#nine-box > .box');
 
 
-	nineSq.addEventListener("mouseover", function(event) {
-		var moveElem = move == CROSS ? XELEM : OELEM;
-		if (event.target.classList.contains('box') && 
-			!event.target.classList.contains('box-disabled') &&
-			!event.target.classList.contains('hovered')) {
-			event.target.insertAdjacentHTML('afterbegin', moveElem);
-			event.target.classList.add('hovered');
-		}
-	});
-	nineSq.addEventListener("mouseout", function(event) {
-		var moveElem = move == CROSS ? XELEM : OELEM;
-		if (event.target.classList.contains('box') && 
-			!event.target.classList.contains('box-disabled') &&
-			event.target.classList.contains('hovered')) {
-			event.target.removeChild(event.target.firstChild);
-			event.target.classList.remove('hovered');
-		}
-	});
-	nineSq.addEventListener("click", function(event) {
-		var moveElem = move == CROSS ? XELEM : OELEM;
-		if (event.target.classList.contains('box') &&
-			!event.target.classList.contains('box-disabled')) {
-			event.target.classList.add('box-disabled');
-			event.target.classList.remove('hovered');
-			console.log(event.target.dataset.row);
-			console.log(event.target.dataset.col);
-			move = !move;
-		}
+	boxes.forEach(function (box, i) {
+		box.addEventListener("mouseover", function(event) {
+			var moveElem = move == CROSS ? XELEM : OELEM;
+			if (!box.classList.contains('box-disabled') &&
+				!box.classList.contains('hovered')) {
+				box.insertAdjacentHTML('afterbegin', moveElem);
+				box.classList.add('hovered');
+			}
+		});
+		box.addEventListener("mouseout", function(event) {
+			var moveElem = move == CROSS ? XELEM : OELEM;
+			if (!box.classList.contains('box-disabled') &&
+				box.classList.contains('hovered')) {
+				box.removeChild(box.firstChild);
+				box.classList.remove('hovered');
+			}
+		});
+		box.addEventListener("click", function(event) {
+			var moveElem = move == CROSS ? XELEM : OELEM;
+			if (!box.classList.contains('box-disabled')) {
+				box.classList.add('box-disabled');
+				box.classList.remove('hovered');
+				console.log(parseInt(i / ROWS));
+				console.log(parseInt(i % COLS));
+				move = !move;
+			}
+		});
 	});
 
 	sInput.addEventListener("click", function() {
